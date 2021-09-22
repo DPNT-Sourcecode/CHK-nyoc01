e53935670b1013efa7f087baddfc2660a13e53f7
sbx_user1051 <sbx_user1051@169.254.20.173>
2021-09-22 18:40:17 +0000

    Wed Sep 22 18:40:17 UTC 2021

--- test/solutions/CHK/test-chk-r1.js
+++ test/solutions/CHK/test-chk-r1.js
@@ -6,6 +6,7 @@ const checkout = require('../../../lib/solutions/CHK/checkout');
 
 describe('CHK challenge: SKUS', function() {
 	it('should return correct value', function() {
-	    assert.equal(checkout("test"), 5);
+	    assert.equal(checkout("A"), 50);
+        assert.equal(checkout(" "), -1);
 	});
 });
