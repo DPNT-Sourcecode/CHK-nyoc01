1d3a87c4dde55ef8d1e444238e3ee66ca2963551
sbx_user1051 <sbx_user1051@169.254.20.173>
2021-09-22 18:29:58 +0000

    Wed Sep 22 18:29:58 UTC 2021

--- test/solutions/CHK/test-chk-r1.js
+++ test/solutions/CHK/test-chk-r1.js
@@ -2,10 +2,10 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const sum = require('../../../lib/solutions/SUM/sum');
+const checkout = require('../../../lib/solutions/CHK/checkout');
 
 describe('CHK challenge: SKUS', function() {
-	it('should return 3, which is the sum of 1 and 2', function() {
-	    assert.equal(sum(1, 2), 3);
+	it('should return correct value', function() {
+	    assert.equal(checkout("test"), 5);
 	});
 });
